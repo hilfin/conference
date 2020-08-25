35f6c94fa4eee8d0f82a68300dfded7643929e01
hilfin <support@hilfintechnology.com>
2020-08-26 00:44:04 +0530

    test new profile auto deployment

--- jitsi-meet/react/features/settings/components/web/ProfileTab.js
+++ jitsi-meet/react/features/settings/components/web/ProfileTab.js
@@ -129,7 +129,7 @@ class ProfileTab extends AbstractDialogTab<Props> {
                                 ({ target: { value } }) =>
                                     super._onChange({ email: value })
                             }
-                            placeholder = 'George'
+                            placeholder = 'Hilfin'
                             shouldFitContainer = { true }
                             type = 'text'
                             value = { email } />
